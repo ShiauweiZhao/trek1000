7ad77baf2c7ab3994c898e4900d4f0d6b57e2f85
zhaoxiaowei <zhaoxiaowei@zerozero.cn>
2018-05-28 21:08:04 +0800

    修改uwb.cpp

--- uwb.cpp
+++ uwb.cpp
@@ -1,18 +1,40 @@
 #include<fstream>
 #include<iostream>
 #include<string>
-
+#include<iomanip>
+#include<unistd.h>
 using namespace std;
+#define _MAX_PATH 300
+
 
 
 int main()
 {
   string s;
   ifstream infine;
-  infine.open("//Users//zhaoxiaowei//Documents//uwb//log6.txt");
-  ofstream outf;
-  outf.open("//Users//zhaoxiaowei//Documents//uwb//log66.txt");
-  while(getline(infine,s))
+  string filestr;
+  string filestr1;
+  string logpath;
+  string logoutpath;
+  logpath="//data//trek1000//log1.txt";
+  logoutpath="//data//trek1000//log111.txt";
+     char buffer[_MAX_PATH];  
+    if(! getcwd(buffer,_MAX_PATH))
+    {
+	    cout<<"error"<<endl;
+    }else
+    cout<<buffer<<endl;
+    filestr=buffer;
+       filestr1=buffer;
+
+    filestr+=logpath;
+    infine.open(filestr);
+    cout<<filestr<<endl;
+   ofstream outf;
+   filestr1+=logoutpath;
+   outf.open(filestr1);
+   cout<<filestr1<<endl;
+   while(getline(infine,s))
  {
   if(s.find("TS")!=string::npos)
  {
